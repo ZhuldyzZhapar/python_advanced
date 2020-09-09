32cd76139bac4e66605b84f3bf4daecead1b162a
Timur Bakibayev <timurbakibayev@gmail.com>
2020-09-09 13:15:00 +0200

    substitution cipher

---
+++ substitution.py
@@ -0,0 +1,25 @@
+# You are given:
+# 1. A source text (input)
+# 2. Alphabet (alpha = "abcdefgh....xyz")
+# 3. Key (key = "qwertzuioplkjhgfdsayxcvbnm")
+# len(key) == len(alpha)
+# no repeating letters in alpha or key
+
+def encode(text):
+    encoded_text = ""
+    # substitute all letters from the alphabet to
+    # the corresponding letters in the key
+    return encoded_text
+
+
+def decode(encoded_text):
+    text = ""
+    # substitute all letters from the key to
+    # the corresponding letters in the alphabet
+    return text
+
+# input: 0 - encode/1 - decode?
+# text
+# print(encoded/decoded text)#
+
+Hello, world! -> Itffk, dksfo!
